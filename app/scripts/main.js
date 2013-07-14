3ec57040c1e8c453165dbe0281a9af53c6496fdc
Jeff Senkevich <spoatacus@gmail.com>
2013-07-14 17:09:36 -0400

    Use rickshaw ajax graph

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,73 +25,65 @@ function ctof(temp) {
 
 $(document).ready(function() {
     // get us some data
-    $.ajax(config.api_root + '/reading/latest', {
-        dataType: 'json'
-    }).success(function(data) {
-        var chart_data = [],
-            graph;
+    var x_axis = null,
+        y_axis = null,
+        hoverDetail = null,
+        graph = null;
 
-        // or data is newest to oldest, flip it around
-        data = data.reverse();
+    graph = new Rickshaw.Graph.Ajax({
+        element: document.querySelector('#chart_temperature'),
+        dataURL: config.api_root + '/reading/latest',
+        renderer: 'line',
+        min: 70,
+        max: 80,
+        onData: function(d) {
+            var data = [{
+                'color': 'steelblue',
+                'name': 'Temperature',
+                'data': []
+            }];
 
-        $.each(data, function(index, item) {
-            chart_data.push({x: moment(item[4]).unix(), y: ctof(item[3])});
-        });
+            // our data is newest to oldest, flip it around
+            d = d.reverse();
 
-        // update latest
-        updateLatest(data[data.length-1]);
-
-        graph = new Rickshaw.Graph({
-            element: document.querySelector('#chart_temperature'),
-//            width: 540,
-//            height: 300,
-            min: 70,
-            max: 85,
-            renderer: 'line',
-            series: [{
-                color: 'steelblue',
-                data: chart_data,
-                name: 'Temperature'
-            }]
-        });
-
-        var x_axis = new Rickshaw.Graph.Axis.Time({graph: graph});
-
-        var y_axis = new Rickshaw.Graph.Axis.Y({
-            graph: graph
-        });
-
-        var hoverDetail = new Rickshaw.Graph.HoverDetail({
-            graph: graph
-        });
-
-        graph.render();
-
-        setInterval(function() {
-            $.ajax(config.api_root + '/reading/latest', {
-                dataType: 'json',
-                data: {
-                    limit: 1
-                }
-            }).success(function(data) {
-                // we get an array back with only one item
-                data = data[0];
-
-                // update the latest info
-                updateLatest(data);
-
-                var secs = moment(data[4]).unix(),
-                    series = graph.series[0].data;
-
-                // add new reading
-                series.push({x: secs, y: ctof(data[3])});
-
-                // remove oldest reading
-                // so we don't hang on to a bunch of data points we don't need
-                graph.series[0].data = series.slice(1,series.length);
-
-                graph.render();
+            // format data for rickshaw
+            $.each(d, function(index, item) {
+                data[0].data.push({x: moment(item[4]).unix(), y: ctof(item[3])});
             });
-        }, 60*1000);
+
+            // update latest
+            updateLatest(d[d.length-1]);
+
+            return data;
+        },
+        onComplete: function(transport) {
+            if (!x_axis) {
+                x_axis = new Rickshaw.Graph.Axis.Time({
+                    graph: transport.graph
+                });
+            }
+
+            if (!y_axis) {
+                y_axis = new Rickshaw.Graph.Axis.Y({
+                    graph: transport.graph
+                });
+            }
+
+            if (!hoverDetail) {
+                hoverDetail = new Rickshaw.Graph.HoverDetail({
+                    graph: transport.graph
+                });
+            }
+
+            transport.graph.render();
+        },
+        series: [{
+            name: 'Temperature'
+        }]
     });
+
+    // update the graph every 60 seconds
+    setInterval(function() {
+        graph.request();
+    }, 60*1000);
 });
